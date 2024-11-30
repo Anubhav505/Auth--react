2e38aec6109fa0fb029f6e9781b99a2f8c9d2a44
Anubhav <xcaliber704@gmail.com>
2024-11-30 15:16:31 +0530

    project added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Login.jsx'
-import Signup from "./Signup.jsx";
-import Home from './Home.jsx'
-import Dashboard from './Dashboard.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router,Routes,Route,Navigate,} from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-createRoot(document.getElementById('root')).render(
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import Home from "./Home.jsx";
+import Dashboard from "./Dashboard.jsx";
+
+const ProtectedRoute = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/user", { withCredentials: true })
+      .then((response) => {
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+        }
+      })
+      .catch(() => setIsAuthenticated(false));
+}, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={< Home />}/>
-        <Route path='/dashboard' element={<Dashboard />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<Signup />}/>   
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
